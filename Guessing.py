f3d732f067232b4ebea1218db41e82e59b4e19e8
Incognitin <139005180+Incognitin@users.noreply.github.com>
2023-07-20 02:16:34 -0400

    Add files via upload

---
+++ Guessing.py
@@ -0,0 +1,16 @@
+import random
+
+comp = random.randint(0,10)
+play_game = True
+while play_game == True:
+    player = int(input("guess your number: "))
+    if player == comp:
+        print("You win!")
+        play_game == False
+        break
+    else:
+        if player > comp:
+            print("The number should be lower: ")
+        elif player < comp:
+            print("The number should be higher: ")
+
