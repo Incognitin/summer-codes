54d4dec193c48ea35bb7ec3842ba5e357e2ff47d
Incognitin <139005180+Incognitin@users.noreply.github.com>
2023-07-20 01:35:16 -0400

    Hangman.py

--- hangman.py
+++ hangman.py
@@ -1,9 +1,9 @@
 import time
 import random
 
+# holds the words for the game to be printed
 hangman_words = ["cacophony", "cognition", "dissertation", "epiphany", "exponential", "meticulous", "intellectual",
                  "nominal", "crazy", "pneumonoultramicroscopicsilicovolcanoconiosis"]
-
 
 def hangman():
     print("Let's play hangman!!\n ")
@@ -14,27 +14,19 @@ def hangman():
     guessed = []
     old_len = len(guessed)
     play_game = True
-    # print(word_list)
     print(display_list)
     print("This word has {} letters in it\n".format(length))
     while play_game == True:
         Guess = str(input("Enter your guess(Only letters!!)\n"))
-        # for letter in word_list:
-        # print("")
         for num in range(length):
             if Guess == word_list[num]:
                 display_list = display_list[:num] + Guess + display_list[num + 1:]
                 guessed.append(Guess)
                 time.sleep(1)
                 print(display_list)
-                # print(word_list)
-                # Guess = str(input("Enter your guess(Only letters!!)"))
-
-
+              
             elif Guess not in word_list:
                 count += 1
-                # return(count)
-                #Guess = str(input("Enter your guess(Only letters!!)"))
 
             while word_list == list(display_list) or list(Guess) == word_list:
                 play_game = False
@@ -51,7 +43,6 @@ def hangman():
                         "  |    / \ \n"
                         "__|__\n"
                         "Wrong guess. You are hanged!!!\n")
-                # return("Wrong guess. You are hanged!!!\n")
 
 
 print(hangman())
